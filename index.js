cfb4f8f2dffbfadaadc90a57b198182a6bb509a2
sompop <sompop.kulapalanont@gmail.com>
2020-06-10 11:51:22 +0700

    update the forbidden function

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const restrictedWords = require("./restrictedWords.json");//if can require
 exports.restrictedWords = restrictedWords;
-exports.isForbiddenName = (name) => restrictedWords.findIndex(name) >= 0
+exports.isForbiddenName = (name) => restrictedWords.findIndex((word) => word === name ) >= 0
